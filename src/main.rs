66ce2d6f276a7c3dcc03a3926bece5393c652ec2
Cesar Oliveira <cesar.oliveira@totvs.com.br>
2024-04-05 11:47:54 -0300

    ✨ (main.rs): add functionality to compare two numbers input by the user and print the greater number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
+fn convert_to_int(s: & String) -> i32 {
+    let x = s.trim().parse::<i32>().unwrap();
+    x
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut number1 = String::new();
+    io::stdin().read_line(&mut number1).expect("Failed to read number 1");
+
+    let mut number2 = String::new();
+    io::stdin().read_line(&mut number2).expect("Failed to read number 2");
+
+    if convert_to_int(&number1) > convert_to_int(&number2) {
+        println!("{} is greater than {}", number1, number2);
+    } else {
+        println!("{} is greater than {}", number2, number1);
+    }
 }
